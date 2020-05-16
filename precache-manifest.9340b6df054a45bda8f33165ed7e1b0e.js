f73006fbc5c15cb96761d4f566bf8abd6d0509c8
Edwin Torres <codesandtags@gmail.com>
2020-05-16 16:41:41 -0500

    Updates

---
+++ precache-manifest.9340b6df054a45bda8f33165ed7e1b0e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e8c79fe0ab96617522c930b24df835",
+    "url": "/zemoga-ui-test/index.html"
+  },
+  {
+    "revision": "f3cb2e6f1dcc88545d21",
+    "url": "/zemoga-ui-test/static/css/main.2e80a0f6.chunk.css"
+  },
+  {
+    "revision": "ab622ddb27cc94820195",
+    "url": "/zemoga-ui-test/static/js/2.7150d177.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zemoga-ui-test/static/js/2.7150d177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3cb2e6f1dcc88545d21",
+    "url": "/zemoga-ui-test/static/js/main.e0122b35.chunk.js"
+  },
+  {
+    "revision": "19ee8acdb37b6771c07e",
+    "url": "/zemoga-ui-test/static/js/runtime-main.4830836a.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/zemoga-ui-test/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/zemoga-ui-test/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/zemoga-ui-test/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/zemoga-ui-test/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/zemoga-ui-test/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/zemoga-ui-test/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/zemoga-ui-test/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/zemoga-ui-test/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/zemoga-ui-test/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/zemoga-ui-test/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/zemoga-ui-test/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/zemoga-ui-test/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/zemoga-ui-test/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
